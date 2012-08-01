c8cb7da35685c71538b459c1323cffedb71b4227
Kevin Liu <kliu5@marvell.com>
2012-08-01 18:51:03 +0800

    mmc: print bus speed and width for new cards
    print the bus speed and width for the new added card, easier to
    check the working mode of the card, for debug purpose.
    
    Change-Id: Ia3205bd81610cc5fda16fc62fbb33593c40c5345
    Signed-off-by: Bin Wang <binw@marvell.com>
    Signed-off-by: Kevin Liu <kliu5@marvell.com>

--- drivers/mmc/core/bus.c
+++ drivers/mmc/core/bus.c
@@ -360,6 +360,10 @@ int mmc_add_card(struct mmc_card *card)
 			(mmc_card_hs200(card) ? "HS200 " : ""),
 			mmc_card_ddr_mode(card) ? "DDR " : "",
 			uhs_bus_speed_mode, type, card->rca);
+		pr_info("%s: ios: %d hz, %d bit width\n",
+			mmc_hostname(card->host),
+			card->host->ios.clock,
+			1 << card->host->ios.bus_width);
 	}
 
 #ifdef CONFIG_DEBUG_FS
