c2c0ea81752acdaac16694a70023a47c90866337
Nenghua Cao <nhcao@marvell.com>
2014-01-10 11:27:34 +0800

    ASoC: dpcm: don't do hw_param when BE has done hw_param
        It fixes the following case:
        Two FEs connects the same BE; FE1 & BE path has been opened and hw_paramed.
    At this momment, FE2 & BE path is being opened and hw_paramed. The BE
    dai will do hw_param again even if it has done hw_param. It is not
    reasonable.
    FE1------------>BE
    FE2-------------^
    
    Change-Id: Ia6b283514979744cc10fab063afb953461269cc8
    Signed-off-by: Nenghua Cao <nhcao@marvell.com>

--- sound/soc/soc-pcm.c
+++ sound/soc/soc-pcm.c
@@ -1455,7 +1455,6 @@ int dpcm_be_dai_hw_params(struct snd_soc_pcm_runtime *fe, int stream)
 			continue;
 
 		if ((be->dpcm[stream].state != SND_SOC_DPCM_STATE_OPEN) &&
-		    (be->dpcm[stream].state != SND_SOC_DPCM_STATE_HW_PARAMS) &&
 		    (be->dpcm[stream].state != SND_SOC_DPCM_STATE_HW_FREE))
 			continue;
 
@@ -1504,7 +1503,6 @@ unwind:
 			continue;
 
 		if ((be->dpcm[stream].state != SND_SOC_DPCM_STATE_OPEN) &&
-		   (be->dpcm[stream].state != SND_SOC_DPCM_STATE_HW_PARAMS) &&
 		   (be->dpcm[stream].state != SND_SOC_DPCM_STATE_HW_FREE) &&
 		   (be->dpcm[stream].state != SND_SOC_DPCM_STATE_STOP))
 			continue;
