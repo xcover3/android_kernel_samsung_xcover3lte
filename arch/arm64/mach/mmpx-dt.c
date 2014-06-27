d28d3e4dddeb75ab52f52c8425c31d266de2229e
Chao Xie <chao.xie@marvell.com>
2014-06-27 16:08:17 +0800

    arm64: add pxa1928 support
    Add pxa1928 machine descriptor at mmpx-dt.c.
    Add clock dtsi, soc dtsi and dts for pxa1928.
    
    Change-Id: I2e5942c5f7a2ea604166bbd4e00e4e355442e2b2
    Signed-off-by: Chao Xie <chao.xie@marvell.com>

---
+++ arch/arm64/mach/mmpx-dt.c
@@ -0,0 +1,119 @@
+/*
+ *  linux/arch/arm64/mach/mmpx-dt.c
+ *
+ *  Copyright (C) 2012 Marvell Technology Group Ltd.
+ *  Author: Haojian Zhuang <haojian.zhuang@marvell.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License version 2 as
+ *  publishhed by the Free Software Foundation.
+ */
+
+#include <linux/init.h>
+#include <linux/io.h>
+#include <linux/of_platform.h>
+#include <linux/clocksource.h>
+
+#include <asm/mach/arch.h>
+
+#include "mmpx-dt.h"
+
+#define MPMU_PHYS_BASE		0xd4050000
+#define GEN_TMR2_PHYS_BASE	0xd4080000
+#define MPMU_PRR_SP		0x0020
+#define MPMU_WDTPCR		0x0200
+#define MPMU_WDTPCR1		0x0204
+#define MPMU_PRR_PJ		0x1020
+
+#define MPMU_PRR_SP_WDTR	(1 << 4)
+#define MPMU_PRR_SP_CPR		(1 << 0)
+
+#define TMR_WFAR               (0x009c)
+#define TMR_WSAR               (0x00A0)
+
+#define GEN_TMR_CFG            (0x00B0)
+#define GEN_TMR_LD1            (0x00B8)
+
+/* Get SoC Access to Generic Timer */
+static void arch_timer_soc_access_enable(void __iomem *gen_tmr_base)
+{
+	__raw_writel(0xbaba, gen_tmr_base + TMR_WFAR);
+	__raw_writel(0xeb10, gen_tmr_base + TMR_WSAR);
+}
+
+static void arch_timer_soc_config(void __iomem *mpmu_base)
+{
+	void __iomem *gen_tmr2_base;
+	u32 tmp;
+
+	gen_tmr2_base = ioremap(GEN_TMR2_PHYS_BASE, SZ_4K);
+	if (!gen_tmr2_base) {
+		pr_err("ioremap gen_tmr_base failed\n");
+		return;
+	}
+
+	/* Enable WDTR2*/
+	tmp  = __raw_readl(mpmu_base + MPMU_PRR_SP);
+	tmp = tmp | MPMU_PRR_SP_WDTR;
+	__raw_writel(tmp, mpmu_base + MPMU_PRR_SP);
+
+	/* Initialize Counter to zero */
+	arch_timer_soc_access_enable(gen_tmr2_base);
+	__raw_writel(0x0, gen_tmr2_base + GEN_TMR_LD1);
+
+	/* Program Generic Timer Clk Frequency */
+	arch_timer_soc_access_enable(gen_tmr2_base);
+	tmp = __raw_readl(gen_tmr2_base + GEN_TMR_CFG);
+	tmp |= (3 << 4); /* 3.25MHz/32KHz Counter auto switch enabled */
+	arch_timer_soc_access_enable(gen_tmr2_base);
+	__raw_writel(tmp, gen_tmr2_base + GEN_TMR_CFG);
+
+	/* Start the Generic Timer Counter */
+	arch_timer_soc_access_enable(gen_tmr2_base);
+	tmp = __raw_readl(gen_tmr2_base + GEN_TMR_CFG);
+	tmp |= 0x3;
+	arch_timer_soc_access_enable(gen_tmr2_base);
+	__raw_writel(tmp, gen_tmr2_base + GEN_TMR_CFG);
+
+	iounmap(gen_tmr2_base);
+}
+
+static __init void pxa1928_timer_init(void)
+{
+	void __iomem *mpmu_base;
+
+	mpmu_base = ioremap(MPMU_PHYS_BASE, SZ_4K);
+	if (!mpmu_base) {
+		pr_err("ioremap mpmu_base failed");
+		return;
+	}
+
+#ifdef CONFIG_ARM_ARCH_TIMER
+	arch_timer_soc_config(mpmu_base);
+#endif
+	/*
+	 * bit 7.enable wdt reset from thermal
+	 * bit 6.enable wdt reset from timer2
+	 */
+	__raw_writel(0xD3, mpmu_base + MPMU_WDTPCR);
+
+	iounmap(mpmu_base);
+
+	clocksource_of_init();
+}
+
+static __init void pxa1928_clk_init(void)
+{
+	mmp_clk_of_init();
+}
+
+static const char *pxa1928_dt_board_compat[] __initdata = {
+	"marvell,pxa1928",
+	NULL,
+};
+
+DT_MACHINE_START(PXA1928_DT, "PXA1928")
+	.init_time      = pxa1928_timer_init,
+	.init_clk	= pxa1928_clk_init,
+	.dt_compat      = pxa1928_dt_board_compat,
+MACHINE_END
