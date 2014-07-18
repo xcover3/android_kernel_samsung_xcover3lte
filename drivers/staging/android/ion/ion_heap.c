7de29f3a84304c1c8e475f75665558f67e8d010f
Lisa Du <cldu@marvell.com>
2014-07-18 18:47:53 +0800

    ion: Add private buffer flag to skip page pooling on free
    Currently, when we free a buffer it might actually just go back into a
    heap-specific page pool rather than going back to the system. This poses
    a problem because sometimes (like when we're running a shrinker in low
    memory conditions) we need to force the memory associated with the
    buffer to truly be relinquished to the system rather than just going
    back into a page pool.
    
    There isn't a use case for this flag by Ion clients, so make it a
    private flag. The main use case right now is to provide a mechanism for
    the deferred free code to force stale buffers to bypass page pooling.
    
    Change-Id: Ia108969a49930e0cdc5290aa00f7ac90a9347039
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>
    Signed-off-by: Lisa Du <cldu@marvell.com>

--- drivers/staging/android/ion/ion_heap.c
+++ drivers/staging/android/ion/ion_heap.c
@@ -178,7 +178,8 @@ size_t ion_heap_freelist_size(struct ion_heap *heap)
 	return size;
 }
 
-size_t ion_heap_freelist_drain(struct ion_heap *heap, size_t size)
+static size_t _ion_heap_freelist_drain(struct ion_heap *heap, size_t size,
+				bool skip_pools)
 {
 	struct ion_buffer *buffer;
 	size_t total_drained = 0;
@@ -197,6 +198,8 @@ size_t ion_heap_freelist_drain(struct ion_heap *heap, size_t size)
 					  list);
 		list_del(&buffer->list);
 		heap->free_list_size -= buffer->size;
+		if (skip_pools)
+			buffer->private_flags |= ION_PRIV_FLAG_SHRINKER_FREE;
 		total_drained += buffer->size;
 		spin_unlock(&heap->free_lock);
 		ion_buffer_destroy(buffer);
@@ -205,6 +208,16 @@ size_t ion_heap_freelist_drain(struct ion_heap *heap, size_t size)
 	spin_unlock(&heap->free_lock);
 
 	return total_drained;
+}
+
+size_t ion_heap_freelist_drain(struct ion_heap *heap, size_t size)
+{
+	return _ion_heap_freelist_drain(heap, size, false);
+}
+
+size_t ion_heap_freelist_shrink(struct ion_heap *heap, size_t size)
+{
+	return _ion_heap_freelist_drain(heap, size, true);
 }
 
 static int ion_heap_deferred_free(void *data)
