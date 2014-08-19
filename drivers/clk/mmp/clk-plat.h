18079da35b834bc9e64018f4aad3cab0695b26c1
Zhoujie Wu <zjwu@marvell.com>
2014-08-19 16:10:59 +0800

    clk: mmp: remove dvc driver dependency of CONFIG_CPU_PXA988
    Previously hwdvc driver depended on CONFIG_CPU_PXA988, now remove
    the dependency as it will be used for ULC and helan3, which won't
    have this config.
    In dvfs-dvc.c, there are some initcall functions, will be called on
    multi platforms, but they will do nothing in case dvc_info is null when
    some platforms don't use this driver.
    
    For platform hwdvc register file dvfs-pxa1u88.c and dvfs-pxa1L88.c
    also could be built as Y due to only corresponding platform will call
    correct setup function.
    
    Change-Id: I8a2a5a367ae0d5e39f189dfc6859ec47c59af72d
    Signed-off-by: Zhoujie Wu <zjwu@marvell.com>

--- drivers/clk/mmp/clk-plat.h
+++ drivers/clk/mmp/clk-plat.h
@@ -2,10 +2,8 @@
 #define __MACH_MMP_CLK_PLAT_H
 
 #ifdef CONFIG_PXA_DVFS
-#ifdef CONFIG_CPU_PXA988
 extern int setup_pxa1u88_dvfs_platinfo(void);
 extern int setup_pxa1L88_dvfs_platinfo(void);
-#endif
 extern int ddr_get_dvc_level(int rate);
 #endif
 
