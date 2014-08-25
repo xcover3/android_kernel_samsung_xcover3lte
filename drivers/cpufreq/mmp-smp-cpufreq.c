0079711d56422c1a9e123b6f9c4f074026b6f847
Zhoujie Wu <zjwu@marvell.com>
2014-08-25 17:20:27 +0800

    cpufreq: add priority control of min/max/qosmin/qosmax for mmp
    We have four requirements to limit cpu frequency now.
    Policy->min and policy->max are controlled by userspace policy
    like powerhint.
    Qosmin and Qosmax are controlled by kernel driver to limit the
    frequency, like thermal has max cpu frequency limitation.
    
    The priority is defined as below:
    1) qosmin&qosmax > policy->min&policy->max
    2) qosmax > qosmin
    3) policy->max > policy->min
    
    Change-Id: Ie796f1aa42f853c865a3ab38d43ff83e9787963a
    Signed-off-by: Zhoujie Wu <zjwu@marvell.com>

--- drivers/cpufreq/mmp-smp-cpufreq.c
+++ drivers/cpufreq/mmp-smp-cpufreq.c
@@ -39,8 +39,14 @@ static DEFINE_MUTEX(mmp_cpu_lock);
 static bool is_suspended;
 static struct cpufreq_frequency_table *freq_table;
 
+/* Qos min request client cpufreq driver policy->cpuinfo.min */
 static struct pm_qos_request cpufreq_qos_req_min = {
 	.name = "cpu_freqmin",
+};
+
+/* Qos max request client cpufreq driver policy->cpuinfo.max */
+static struct pm_qos_request cpufreq_qos_req_max = {
+	.name = "cpu_freqmax",
 };
 
 static int mmp_update_cpu_speed(unsigned long rate)
@@ -72,11 +78,86 @@ static int mmp_update_cpu_speed(unsigned long rate)
 	return ret;
 }
 
+/* Same as cpufreq_frequency_table_target, but no policy min/max check */
+int cpufreq_table_target(struct cpufreq_policy *policy,
+			 struct cpufreq_frequency_table *table,
+			 unsigned int target_freq,
+			 unsigned int relation, unsigned int *index)
+{
+	struct cpufreq_frequency_table optimal = {
+		.driver_data = ~0,
+		.frequency = 0,
+	};
+	struct cpufreq_frequency_table suboptimal = {
+		.driver_data = ~0,
+		.frequency = 0,
+	};
+	unsigned int i;
+
+	pr_debug("request for target %u kHz (relation: %u) for cpu %u\n",
+		 target_freq, relation, policy->cpu);
+
+	switch (relation) {
+	case CPUFREQ_RELATION_H:
+		suboptimal.frequency = ~0;
+		break;
+	case CPUFREQ_RELATION_L:
+		optimal.frequency = ~0;
+		break;
+	}
+
+	for (i = 0; (table[i].frequency != CPUFREQ_TABLE_END); i++) {
+		unsigned int freq = table[i].frequency;
+		if (freq == CPUFREQ_ENTRY_INVALID)
+			continue;
+
+		switch (relation) {
+		case CPUFREQ_RELATION_H:
+			if (freq <= target_freq) {
+				if (freq >= optimal.frequency) {
+					optimal.frequency = freq;
+					optimal.driver_data = i;
+				}
+			} else {
+				if (freq <= suboptimal.frequency) {
+					suboptimal.frequency = freq;
+					suboptimal.driver_data = i;
+				}
+			}
+			break;
+		case CPUFREQ_RELATION_L:
+			if (freq >= target_freq) {
+				if (freq <= optimal.frequency) {
+					optimal.frequency = freq;
+					optimal.driver_data = i;
+				}
+			} else {
+				if (freq >= suboptimal.frequency) {
+					suboptimal.frequency = freq;
+					suboptimal.driver_data = i;
+				}
+			}
+			break;
+		}
+	}
+	if (optimal.driver_data > i) {
+		if (suboptimal.driver_data > i)
+			return -EINVAL;
+		*index = suboptimal.driver_data;
+	} else
+		*index = optimal.driver_data;
+
+	pr_debug("target is %u (%u kHz, %u)\n", *index, table[*index].frequency,
+		 table[*index].driver_data);
+
+	return 0;
+}
+
 static int mmp_target(struct cpufreq_policy *policy,
 		      unsigned int target_freq, unsigned int relation)
 {
 	int idx;
-	unsigned int freq;
+	unsigned int freq, qos_min, qos_max;
 	int ret = 0;
 
 	mutex_lock(&mmp_cpu_lock);
@@ -86,12 +167,26 @@ static int mmp_target(struct cpufreq_policy *policy,
 		goto out;
 	}
 
-	target_freq = max_t(unsigned int, pm_qos_request(PM_QOS_CPUFREQ_MIN),
-			    target_freq);
-	cpufreq_frequency_table_target(policy, freq_table, target_freq,
-				       relation, &idx);
-
+	/*
+	 * Policy min and qos min have lower priority
+	 * than policy max and qos max.
+	 * Policy min and qos min has no priority order
+	 * Policy max and qos max has no proirity order
+	 */
+	qos_min = (unsigned int)pm_qos_request(PM_QOS_CPUFREQ_MIN);
+	qos_max = (unsigned int)pm_qos_request(PM_QOS_CPUFREQ_MAX);
+	target_freq = max_t(unsigned int, policy->min, target_freq);
+	target_freq = max_t(unsigned int, qos_min, target_freq);
+	target_freq = min_t(unsigned int, policy->max, target_freq);
+	target_freq = min_t(unsigned int, qos_max, target_freq);
+	if ((target_freq == policy->max) || (target_freq == qos_max))
+		relation = CPUFREQ_RELATION_H;
+	cpufreq_table_target(policy, freq_table, target_freq, relation, &idx);
 	freq = freq_table[idx].frequency;
+	pr_debug("Qos_min:%d Qos_max:%d, Target:%d(KHZ)\n",
+		pm_qos_request(PM_QOS_CPUFREQ_MIN),
+		pm_qos_request(PM_QOS_CPUFREQ_MAX),
+		freq);
 
 	ret = mmp_update_cpu_speed(freq);
 out:
@@ -150,7 +245,7 @@ static int cpufreq_min_notify(struct notifier_block *b,
 	if (freq >= val)
 		return NOTIFY_OK;
 
-	ret = __cpufreq_driver_target(policy, val, CPUFREQ_RELATION_L);
+	ret = mmp_target(policy, val, CPUFREQ_RELATION_L);
 	cpufreq_cpu_put(policy);
 	if (ret < 0)
 		return NOTIFY_BAD;
@@ -171,14 +266,12 @@ static int cpufreq_max_notify(struct notifier_block *b,
 	int cpu = 0;
 
 	freq = cpufreq_generic_get(cpu);
-	if (freq <= val)
-		return NOTIFY_OK;
 
 	policy = cpufreq_cpu_get(cpu);
 	if (!policy)
 		return NOTIFY_BAD;
 
-	ret = __cpufreq_driver_target(policy, val, CPUFREQ_RELATION_H);
+	ret = mmp_target(policy, val, CPUFREQ_RELATION_H);
 	cpufreq_cpu_put(policy);
 	if (ret < 0)
 		return NOTIFY_BAD;
@@ -216,7 +309,10 @@ static int mmp_cpufreq_init(struct cpufreq_policy *policy)
 		pm_qos_add_request(&cpufreq_qos_req_min,
 				   PM_QOS_CPUFREQ_MIN,
 				   policy->cpuinfo.min_freq);
-
+	if (!pm_qos_request_active(&cpufreq_qos_req_max))
+		pm_qos_add_request(&cpufreq_qos_req_max,
+					PM_QOS_CPUFREQ_MAX,
+					policy->cpuinfo.max_freq);
 	return 0;
 }
 
