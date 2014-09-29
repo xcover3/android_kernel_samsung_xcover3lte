97f25de6a47eb3047b9234b78a159fff7b28e20a
Owen Zhang <xinzha@marvell.com>
2014-09-29 14:06:08 +0800

    arm64: mmp: add camera and isp support on pxa1908
    1)adjust raw sensor data
    2)add ccic and soc camera support
    
    Change-Id: If265595dbdfaa41ee7cf252e77dc8edd8f6b8f6a
    Signed-off-by: Owen Zhang <xinzha@marvell.com>

--- arch/arm64/mach/helanx-dt.c
+++ arch/arm64/mach/helanx-dt.c
@@ -30,6 +30,21 @@ static const struct of_dev_auxdata helanx_auxdata_lookup[] __initconst = {
 #ifdef CONFIG_MMP_MAP
 	OF_DEV_AUXDATA("mrvl,mmp-sspa-dai", 0xd128dc00, "mmp-sspa-dai.0", NULL),
 #endif
+#ifdef CONFIG_VIDEO_MV_SC2_MMU
+	OF_DEV_AUXDATA("marvell,mmp-sc2mmu", 0xd420F000, "mv_sc2_mmu.0", NULL),
+#endif
+#ifdef CONFIG_VIDEO_MV_SC2_CCIC
+	OF_DEV_AUXDATA("marvell,mmp-sc2ccic", 0xd420a000, "mv_sc2_ccic.0", NULL),
+	OF_DEV_AUXDATA("marvell,mmp-sc2ccic", 0xd420a800, "mv_sc2_ccic.1", NULL),
+#endif
+#ifdef CONFIG_VIDEO_MV_SC2_CAMERA
+	OF_DEV_AUXDATA("marvell,mv_sc2_camera", 0, "mv_sc2_camera.0", NULL),
+#endif
+#if 0
+	OF_DEV_AUXDATA("soc-camera-pdrv", 0, "soc-camera-pdrv.0", &soc_camera_desc_0),
+	OF_DEV_AUXDATA("soc-camera-pdrv", 1, "soc-camera-pdrv.1", &soc_camera_desc_1),
+	OF_DEV_AUXDATA("soc-camera-pdrv", 2, "soc-camera-pdrv.2", &soc_camera_desc_2),
+#endif
 	OF_DEV_AUXDATA("marvell,mmp-disp", 0xd420b000, "mmp-disp", NULL),
 	{}
 };
