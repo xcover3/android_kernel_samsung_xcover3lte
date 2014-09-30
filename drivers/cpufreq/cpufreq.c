9cf5a9451b4cb8da49878626fa73b83d04d67074
Viresh Kumar <viresh.kumar@linaro.org>
2014-09-30 09:33:17 +0530

    cpufreq: update 'cpufreq_suspended' after stopping governors
    Commit 8e30444e1530 ("cpufreq: fix cpufreq suspend/resume for intel_pstate")
    introduced a bug where the governors wouldn't be stopped anymore for
    ->target{_index}() drivers during suspend. This happens because
    'cpufreq_suspended' is updated before stopping the governors during suspend
    and due to this __cpufreq_governor() would return early due to this check:
    
    	/* Don't start any governor operations if we are entering suspend */
    	if (cpufreq_suspended)
    		return 0;
    
    Change-Id: Icd62d91dc570bad7d806664ec424155016bd5ae5
    Fixes: 8e30444e1530 ("cpufreq: fix cpufreq suspend/resume for intel_pstate")
    Cc: 3.15+ <stable@vger.kernel.org> # 3.15+: 8e30444e1530 "cpufreq: fix cpufreq suspend/resume for intel_pstate"
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Bill Zhou <zhoub@marvell.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1617,10 +1617,8 @@ void cpufreq_suspend(void)
 	if (!cpufreq_driver)
 		return;
 
-	cpufreq_suspended = true;
-
 	if (!has_target())
-		return;
+		goto suspend;
 
 	pr_debug("%s: Suspending Governors\n", __func__);
 
@@ -1633,6 +1631,9 @@ void cpufreq_suspend(void)
 			pr_err("%s: Failed to suspend driver: %p\n", __func__,
 				policy);
 	}
+
+suspend:
+	cpufreq_suspended = true;
 }
 
 /**
