a47fe0c90904224636c935e66a831422ce7a7866
Neil Zhang <zhangwm@marvell.com>
2014-10-28 19:20:56 +0800

    usb: gadget: don't create new string_container if already exist
    Don't create new usb_gadget_string_container if the current strings are
    already exist in the usb_composite_dev.
    Otherwise the ids_tab will overflow soon if we bind / unbind usb
    functions frequently like android does.
    
    Change-Id: Iee5b4c2bd6eaf9bc23817fb64ae30b077a60caf9
    Signed-off-by: Neil Zhang <zhangwm@marvell.com>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -1140,6 +1140,12 @@ struct usb_string *usb_gstrings_attach(struct usb_composite_dev *cdev,
 	if (!n_gstrings)
 		return ERR_PTR(-EINVAL);
 
+	list_for_each_entry(uc, &cdev->gstrings, list) {
+		n_gs = get_containers_gs(uc);
+		if (!strcmp(n_gs[0]->strings[0].s, sp[0]->strings[0].s))
+			return n_gs[0]->strings;
+	}
+
 	uc = copy_gadget_strings(sp, n_gstrings, n_strings);
 	if (IS_ERR(uc))
 		return ERR_PTR(PTR_ERR(uc));
