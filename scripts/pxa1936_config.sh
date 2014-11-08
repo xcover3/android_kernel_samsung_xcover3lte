cd302fcde19f8527909d3d700301bd82e2f34d01
Bill Zhou <zhoub@marvell.com>
2014-11-08 09:09:43 +0800

    scripts: pxa1936: update configs for bring up
    For bring up, we disable various features such
    as cpuidle, cpufreq, cpu hotplug, dvfs, etc,
    but add clock driver.
    
    Change-Id: If598982e74a55e7602d5ecb85abdfdda76ad8a83
    Signed-off-by: Bill Zhou <zhoub@marvell.com>

--- scripts/pxa1936_config.sh
+++ scripts/pxa1936_config.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
 ./scripts/config --set-val NR_CPUS 8
+./scripts/config -d CONFIG_NO_HOTPLUG_POLICY
 ./scripts/config -d CPU_HOTPLUG_POL_STANDALONE
+./scripts/config -d CONFIG_CPU_IDLE
+./scripts/config -d CONFIG_CPU_IDLE_MMP_V8
+./scripts/config -d CONFIG_CPU_FREQ
+./scripts/config -d ARM_MMP_SMP_CPUFREQ
+./scripts/config -d CONFIG_CPU_THERMAL
+./scripts/config -d CONFIG_PXA1928_THERMAL
+./scripts/config -d CONFIG_HELAN2_THERMAL
+./scripts/config -d CONFIG_PXA_DVFS
+./scripts/config -d CONFIG_PM_DEVFREQ
+./scripts/config -e PXA1936_CLK
