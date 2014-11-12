94e55beb3fdc31ab177901fe0ee6ca036f3935ba
Raul Xiong <xjian@marvell.com>
2014-11-12 20:21:59 +0800

    ARM64: disable preemption in machine_shutdown
    Port from one patch for arch/arm/kernel/process.c.
    
    Since the smp call to stop the other cpus are handled in those
    cpus in interrupt context, there's a potential for those smp
    handlers to interrupt threads holding spin locks (such as the
    one a mutex holds).  This prevents those threads from ever
    releasing their spin lock, so if the cpu doing the shutdown
    is allowed to switch to another thread that tries to grab the
    same lock/mutex, we could get into a deadlock (the spin lock
    call is called with preemption disabled in the mutex lock code).
    
    To avoid that possibility, disable preemption before doing the
    smp_send_stop().
    
    Change-Id: I4e360654747dd96f1b612d31637a5d19348e07e3
    Signed-off-by: Raul Xiong <xjian@marvell.com>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -148,6 +148,15 @@ void arch_cpu_idle_dead(void)
 void machine_shutdown(void)
 {
 #ifdef CONFIG_SMP
+	/*
+	 * Disable preemption so we're guaranteed to
+	 * run to power off or reboot and prevent
+	 * the possibility of switching to another
+	 * thread that might wind up blocking on
+	 * one of the stopped CPUs.
+	 */
+	preempt_disable();
+
 	smp_send_stop();
 #endif
 }
