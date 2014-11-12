f9ff61ed09d11f456a091c1ba8edc9ed3cbafada
Xiaoguang Chen <chenxg@marvell.com>
2014-11-12 13:34:08 +0800

    drivers: cpuidle: add pxa1936 cpuidle driver based on psci
    1) define power modes
    2) use psci framework to handle low power mode
    3) only init when "method=psci" is defined on dts
    
    Change-Id: I65f85ee6e606e3354bded1cfd2ba357d1d7a3d9e
    Signed-off-by: Xiaoguang Chen <chenxg@marvell.com>

---
+++ drivers/cpuidle/cpuidle_pxa1936.c
@@ -0,0 +1,151 @@
+/*
+ * Pxa1936(helan3) CPU idle driver.
+ *
+ * Copyright (C) 2014 Marvell Ltd.
+ * Author: Xiaoguang Chen <chenxg@marvell.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/cpuidle.h>
+#include <linux/cpu_pm.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/of.h>
+#include <asm/suspend.h>
+#include <asm/proc-fns.h>
+
+static int arm64_enter_state(struct cpuidle_device *dev,
+			     struct cpuidle_driver *drv, int idx);
+
+struct cpuidle_driver arm64_idle_driver = {
+	.name = "arm64_idle",
+	.owner = THIS_MODULE,
+	.states[0] = {
+		      .enter = arm64_enter_state,
+		      .exit_latency = 18,
+		      .target_residency = 36,
+		      /*
+		       * Use CPUIDLE_FLAG_TIMER_STOP flag to let the cpuidle
+		       * framework handle the CLOCK_EVENT_NOTIFY_BROADCAST_
+		       * ENTER/EXIT when entering idle states.
+		       */
+		      .flags = CPUIDLE_FLAG_TIME_VALID,
+		      .name = "C1",
+		      .desc = "C1: Core internal clock gated",
+		      },
+	.states[1] = {
+		      .enter = arm64_enter_state,
+		      .exit_latency = 20,
+		      .target_residency = 40,
+		      .flags = CPUIDLE_FLAG_TIME_VALID |
+		      CPUIDLE_FLAG_TIMER_STOP,
+		      .name = "C2",
+		      .desc = "C2: Core power down",
+		      },
+	.states[2] = {
+		      .enter = arm64_enter_state,
+		      .exit_latency = 450,
+		      .target_residency = 900,
+		      .flags = CPUIDLE_FLAG_TIME_VALID |
+		      CPUIDLE_FLAG_TIMER_STOP,
+		      .name = "MP2",
+		      .desc = "MP2: Core subsystem power down",
+		      },
+	.states[3] = {
+		      .enter = arm64_enter_state,
+		      .exit_latency = 500,
+		      .target_residency = 1000,
+		      .flags = CPUIDLE_FLAG_TIME_VALID |
+		      CPUIDLE_FLAG_TIMER_STOP,
+		      .name = "D1p",
+		      .desc = "D1p: AP idle state",
+		      },
+	.states[4] = {
+		      .enter = arm64_enter_state,
+		      .exit_latency = 600,
+		      .target_residency = 1200,
+		      .flags = CPUIDLE_FLAG_TIME_VALID |
+		      CPUIDLE_FLAG_TIMER_STOP,
+		      .name = "D1",
+		      .desc = "D1: Chip idle state",
+		      },
+
+	.state_count = 5,
+};
+
+/*
+ * arm64_enter_state - Programs CPU to enter the specified state
+ *
+ * @dev: cpuidle device
+ * @drv: cpuidle driver
+ * @idx: state index
+ *
+ * Called from the CPUidle framework to program the device to the
+ * specified target state selected by the governor.
+ */
+static int arm64_enter_state(struct cpuidle_device *dev,
+			     struct cpuidle_driver *drv, int idx)
+{
+	int ret;
+
+	if (!idx) {
+		/*
+		 * C1 is just standby wfi, does not require CPU
+		 * to be suspended
+		 */
+		cpu_do_idle();
+		return idx;
+	}
+
+	cpu_pm_enter();
+	/*
+	 * Pass C-state index to cpu_suspend which in turn will call
+	 * the CPU ops suspend protocol with index as a parameter
+	 */
+	ret = cpu_suspend(idx);
+	if (ret)
+		pr_warn_once("returning from cpu_suspend %s %d\n",
+			     __func__, ret);
+	/*
+	 * Trigger notifier only if cpu_suspend succeeded
+	 */
+	if (!ret)
+		cpu_pm_exit();
+
+	return idx;
+}
+
+/* Returns non-0 if psci property is found in the dtb */
+static int lpm_psci_method(void)
+{
+	struct device_node *np = of_find_node_by_name(NULL, "lpm_method");
+	const char *str = NULL;
+
+	if (np && !of_property_read_string(np, "method", &str)
+			&& !strcmp(str, "psci"))
+		return 1;
+
+	return 0;
+}
+
+/*
+ * arm64_idle_init
+ *
+ * Registers the arm specific cpuidle driver with the cpuidle
+ * framework. It relies on core code to set-up the driver cpumask
+ * and initialize it to online CPUs.
+ */
+int __init arm64_idle_init(void)
+{
+	if (!of_machine_is_compatible("marvell,pxa1936"))
+		return -ENODEV;
+
+	if (!lpm_psci_method())
+		return -ENODEV;
+
+	return cpuidle_register(&arm64_idle_driver, NULL);
+}
+device_initcall(arm64_idle_init);
