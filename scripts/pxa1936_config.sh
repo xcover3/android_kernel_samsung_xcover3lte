4d2fa8f5e69b6cefe8629af05ce1b38c39b8341b
Bill Zhou <zhoub@marvell.com>
2014-11-24 18:29:23 +0800

    scripts: pxa1936: enable cpufreq/devfreq-ddr
    Enable big.LITTLE cpufreq driver and
    devfreq driver for ddr.
    
    And add full config option name to
    each item to keep the same style.
    
    Change-Id: I7bafa9e07a7de6297551e50b226f40788a14fbef
    Signed-off-by: Bill Zhou <zhoub@marvell.com>

--- scripts/pxa1936_config.sh
+++ scripts/pxa1936_config.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-./scripts/config --set-val NR_CPUS 8
+# NOTE:
+# Keep the full config option name here with *CONFIG_*
+
+./scripts/config --set-val CONFIG_NR_CPUS 8
 ./scripts/config -d CONFIG_NO_HOTPLUG_POLICY
 ./scripts/config -d CPU_HOTPLUG_POL_STANDALONE
 ./scripts/config -d CONFIG_CPU_IDLE
 ./scripts/config -d CONFIG_CPU_IDLE_MMP_V8
-./scripts/config -d CONFIG_CPU_FREQ
-./scripts/config -d ARM_MMP_SMP_CPUFREQ
+./scripts/config -e CONFIG_ARM_MMP_BL_CPUFFREQ
+./scripts/config -d CONFIG_ARM_MMP_SMP_CPUFREQ
 ./scripts/config -d CONFIG_PXA1928_THERMAL
 ./scripts/config -d CONFIG_HELAN2_THERMAL
 ./scripts/config -d CONFIG_PXA_DVFS
-./scripts/config -d CONFIG_PM_DEVFREQ
-./scripts/config -e PXA1936_CLK
+./scripts/config -e CONFIG_PXA1936_CLK
 ./scripts/config -d CONFIG_ANDROID_BINDER_IPC_32BIT
