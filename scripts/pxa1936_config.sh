a780d5e939749926c66b23b350799ff24c364220
Bill Zhou <zhoub@marvell.com>
2014-11-27 18:24:16 +0800

    scripts: pxa1936: disable cpufreq and devfreq
    disable cpufreq and devfreq for helan3
    pre-alpha release.
    
    Change-Id: I9263cdd512afe0bdb94b6b7c1df6a3a348dcb88d
    Signed-off-by: Bill Zhou <zhoub@marvell.com>

--- scripts/pxa1936_config.sh
+++ scripts/pxa1936_config.sh
@@ -8,10 +8,11 @@
 ./scripts/config -d CPU_HOTPLUG_POL_STANDALONE
 ./scripts/config -d CONFIG_CPU_IDLE
 ./scripts/config -d CONFIG_CPU_IDLE_MMP_V8
-./scripts/config -e CONFIG_ARM_MMP_BL_CPUFFREQ
+./scripts/config -d CONFIG_CPU_FREQ
 ./scripts/config -d CONFIG_ARM_MMP_SMP_CPUFREQ
 ./scripts/config -d CONFIG_PXA1928_THERMAL
 ./scripts/config -d CONFIG_HELAN2_THERMAL
 ./scripts/config -d CONFIG_PXA_DVFS
+./scripts/config -d CONFIG_PM_DEVFREQ
 ./scripts/config -e CONFIG_PXA1936_CLK
 ./scripts/config -d CONFIG_ANDROID_BINDER_IPC_32BIT
