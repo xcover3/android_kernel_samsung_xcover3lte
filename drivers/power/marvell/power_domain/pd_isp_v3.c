70f84f8096cb5d0a8dbd0addef7edc3719f6b780
Owen Zhang <xinzha@marvell.com>
2014-12-02 12:44:14 +0800

    power: power_domain: remove ISP/SC2 clock set from power
    ISP/SC2 clock set/enable/disable in ISP/SC2 driver.
    and this recommend to use clock tree.
    
    Change-Id: Ia532571a78479fd3fd4d07d44ce415555843c567
    Signed-off-by: Owen Zhang <xinzha@marvell.com>

--- drivers/power/marvell/power_domain/pd_isp_v3.c
+++ drivers/power/marvell/power_domain/pd_isp_v3.c
@@ -22,10 +22,6 @@
 #define APMU_ISP_CLK_RES_CTRL   0x038
 #define APMU_CSI_CCIC2_CLK_RES_CTRL	0x24
 
-#define read(r)		readl(apmu_base + (r))
-#define write(v, r)	writel((v), apmu_base + (r))
-
-
 #define ISP_AHB_EN ((1 << 21) | (1 << 22))
 
 #define MAX_TIMEOUT	5000
@@ -33,7 +29,6 @@
 #define MMP_PD_POWER_ON_LATENCY		300
 #define MMP_PD_POWER_OFF_LATENCY	20
 
-static void __iomem *apmu_base;
 
 struct mmp_pd_isp_data {
 	int id;
@@ -78,11 +73,9 @@ static int mmp_pd_isp_v3_power_on(struct generic_pm_domain *domain)
 	void __iomem *base = pd->reg_base;
 	u32 val;
 	int ret = 0, loop = MAX_TIMEOUT;
-	unsigned int reg;
 
 	if (pd->clk)
 		clk_prepare_enable(pd->clk);
-	apmu_base = ioremap(0xd4282800, SZ_4K);
 
 	val = __raw_readl(base + APMU_CCIC_CLK_RES_CTRL);
 	val |= ISP_AHB_EN;
@@ -128,67 +121,6 @@ static int mmp_pd_isp_v3_power_on(struct generic_pm_domain *domain)
 		goto out;
 	}
 
-	reg = read(APMU_ISP_CLK_RES_CTRL);
-	write(reg | 1<<17 | 1<<23, APMU_ISP_CLK_RES_CTRL);
-	do reg = read(APMU_ISP_CLK_RES_CTRL);
-	while (reg & (1<<23));
-
-	reg = read(APMU_ISP_CLK_RES_CTRL);
-	write(reg | 1<<2 | 1<<4 | 1<<7 | 1<<1, APMU_ISP_CLK_RES_CTRL);
-	do reg = read(APMU_ISP_CLK_RES_CTRL);
-	while (reg & (1<<7));
-
-	reg = read(APMU_ISP_CLK_RES_CTRL);
-	reg |= (0x3 << 24 | 1<<28);
-	write(reg, APMU_ISP_CLK_RES_CTRL);
-
-	reg = read(APMU_CSI_CCIC2_CLK_RES_CTRL);
-	reg &= ~(0xf<<22);
-	reg |= 0xb<<22;
-	reg |= 0x1<<26;
-	reg |= (0x1<<4);
-	/*CSI clock*/
-	reg &= ~(0x3<<16);
-	reg |=  (0x1<<16);
-	reg &= ~(0x7<<18);
-	reg |=  (0x0<<18);
-	reg |= (0x1<<1);
-	reg |= (1<<7);
-	reg |= (0x1<<15);
-	write(reg, APMU_CSI_CCIC2_CLK_RES_CTRL);
-	do reg = read(APMU_CSI_CCIC2_CLK_RES_CTRL);
-	while (reg & (1<<15));
-
-	/*reg = read(APMU_CCIC_CLK_RES_CTRL);*/
-	reg = 0x0;
-	reg |= (0x3<<21);
-	/*clk4x*/
-	reg |= 0x1<<4;
-	reg &= ~(0x3<<16);
-	reg |= (0x1<<16);
-	reg &= ~(0x7<<18);
-	reg |= (0x1<<1);
-	reg |= (0x1<<15);
-	reg |= (1<<7);
-	write(reg, APMU_CCIC_CLK_RES_CTRL);
-	do reg = read(APMU_CCIC_CLK_RES_CTRL);
-	while (reg & (1<<15));
-
-	/* set phy */
-	reg = read(APMU_CSI_CCIC2_CLK_RES_CTRL);
-	reg |= (0x1<<5);
-	reg |= (0x1<<7);
-	reg |= (0x1<<2);
-	reg |= (0x1<<1);
-	write(reg, APMU_CSI_CCIC2_CLK_RES_CTRL);
-
-	reg = read(APMU_CCIC_CLK_RES_CTRL);
-	reg |= (0x1<<5);
-	reg |= (0x1<<7);
-	reg |= (0x1<<2);
-	reg |= (0x1<<1);
-	write(reg, APMU_CCIC_CLK_RES_CTRL);
-
 out:
 	if (pd->clk)
 		clk_disable_unprepare(pd->clk);
@@ -204,7 +136,6 @@ static int mmp_pd_isp_v3_power_off(struct generic_pm_domain *domain)
 	void __iomem *base = pd->reg_base;
 	u32 val;
 	int loop;
-	unsigned int reg;
 
 	spin_lock(&mmp_pd_apmu_lock);
 
@@ -232,45 +163,6 @@ static int mmp_pd_isp_v3_power_off(struct generic_pm_domain *domain)
 		dev_err(pd->dev, "power off timeout\n");
 		return -EBUSY;
 	}
-
-	/*reset and disable ahb clock*/
-	reg = read(APMU_CCIC_CLK_RES_CTRL);
-	reg &= ~(0x3<<21);
-	write(reg, APMU_CCIC_CLK_RES_CTRL);
-	/*reset and clk4x clock*/
-	reg = read(APMU_CCIC_CLK_RES_CTRL);
-	reg &= ~(0x1<<1 | 0x1<<4);
-	write(reg, APMU_CCIC_CLK_RES_CTRL);
-	/*reset and dphy4x clock*/
-	reg = read(APMU_CCIC_CLK_RES_CTRL);
-	reg &= ~(0x1<<2 | 0x1<<5);
-	write(reg, APMU_CCIC_CLK_RES_CTRL);
-
-	/*reset and disable mcu clock*/
-	reg = read(APMU_ISP_CLK_RES_CTRL);
-	reg &= ~(0x3<<27);
-	write(reg, APMU_ISP_CLK_RES_CTRL);
-	/*reset and disable bus clock*/
-	reg = read(APMU_ISP_CLK_RES_CTRL);
-	reg &= ~(0x3<<16);
-	write(reg, APMU_ISP_CLK_RES_CTRL);
-	/*reset and disable isp clock*/
-	reg = read(APMU_ISP_CLK_RES_CTRL);
-	reg &= ~(0x3<<0);
-	write(reg, APMU_ISP_CLK_RES_CTRL);
-
-	/*reset and disable phy clock*/
-	reg = read(APMU_CSI_CCIC2_CLK_RES_CTRL);
-	reg &= ~(0x1<<2 | 0x1<<5);
-	write(reg, APMU_CSI_CCIC2_CLK_RES_CTRL);
-	/*reset and disable csi clock*/
-	reg = read(APMU_CSI_CCIC2_CLK_RES_CTRL);
-	reg &= ~(0x1<<1 | 0x1<<4);
-	write(reg, APMU_CSI_CCIC2_CLK_RES_CTRL);
-	/*disable vclk clock*/
-	reg = read(APMU_CSI_CCIC2_CLK_RES_CTRL);
-	reg &= ~(0x1<<26);
-	write(reg, APMU_CSI_CCIC2_CLK_RES_CTRL);
 
 	return 0;
 }
