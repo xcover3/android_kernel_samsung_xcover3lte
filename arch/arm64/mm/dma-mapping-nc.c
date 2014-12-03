b2ec9c369d7155f37cb54709076356f77cb3866e
Lisa Du <cldu@marvell.com>
2014-12-03 14:47:24 +0800

    arm64: mm: add atomic pool for dma allocations
    CMA allocations don't support atomic allocations.
    Add a dedicated atomic pool to support this.
    
    Change-Id: Ib9af966075cd4db90a6e8c988e6305b49d53ccc5
    Signed-off-by: Lisa Du <cldu@marvell.com>

--- arch/arm64/mm/dma-mapping-nc.c
+++ arch/arm64/mm/dma-mapping-nc.c
@@ -11,12 +11,61 @@
 
 #include <linux/gfp.h>
 #include <linux/export.h>
+#include <linux/genalloc.h>
 #include <linux/slab.h>
 #include <linux/dma-mapping.h>
 #include <linux/dma-contiguous.h>
 #include <linux/vmalloc.h>
 
 #include <asm/cacheflush.h>
+
+static struct gen_pool *atomic_pool;
+
+#define DEFAULT_DMA_COHERENT_POOL_SIZE  SZ_256K
+static size_t atomic_pool_size = DEFAULT_DMA_COHERENT_POOL_SIZE;
+
+static int __init early_coherent_pool(char *p)
+{
+	atomic_pool_size = memparse(p, &p);
+	return 0;
+}
+early_param("coherent_pool", early_coherent_pool);
+
+static void *__alloc_from_pool(size_t size, struct page **ret_page)
+{
+	unsigned long val;
+	void *ptr = NULL;
+
+	if (!atomic_pool) {
+		WARN(1, "coherent pool not initialised!\n");
+		return NULL;
+	}
+
+	val = gen_pool_alloc(atomic_pool, size);
+	if (val) {
+		phys_addr_t phys = gen_pool_virt_to_phys(atomic_pool, val);
+
+		*ret_page = phys_to_page(phys);
+		ptr = (void *)val;
+	}
+
+	return ptr;
+}
+
+static bool __in_atomic_pool(void *start, size_t size)
+{
+	return addr_in_gen_pool(atomic_pool, (unsigned long)start, size);
+}
+
+static int __free_from_pool(void *start, size_t size)
+{
+	if (!__in_atomic_pool(start, size))
+		return 0;
+
+	gen_pool_free(atomic_pool, (unsigned long)start, size);
+
+	return 1;
+}
 
 static void *arm64_dma_alloc(
 		struct device *dev,
@@ -33,7 +82,17 @@ static void *arm64_dma_alloc(
 	size = PAGE_ALIGN(size);
 	order = get_order(size);
 
-	if (cma_available)
+	if (!(gfp & __GFP_WAIT)) {
+		ptr = __alloc_from_pool(size, &page);
+
+		if (ptr) {
+			phys_addr = page_to_phys(page);
+			*dma_handle = phys_to_dma(dev, phys_addr);
+			return ptr;
+		}
+	}
+
+	if (cma_available && (gfp & __GFP_WAIT))
 		page = dma_alloc_from_contiguous(dev,
 				size >> PAGE_SHIFT, order);
 	else
@@ -64,6 +123,9 @@ static void arm64_dma_free(
 	phys_addr_t phys_addr = dma_to_phys(dev, dma_handle);
 	struct page *page = phys_to_page(phys_addr);
 	int order;
+
+	if (__free_from_pool(vaddr, size))
+		return;
 
 	size = PAGE_ALIGN(size);
 	order = get_order(size);
@@ -223,6 +285,73 @@ EXPORT_SYMBOL(arm64_dma_ops);
 
 struct dma_map_ops *dma_ops = &arm64_dma_ops;
 EXPORT_SYMBOL(dma_ops);
+
+static int __init atomic_pool_init(void)
+{
+	unsigned long nr_pages = atomic_pool_size >> PAGE_SHIFT;
+	struct page *page;
+	void *addr;
+	phys_addr_t phys_addr;
+	unsigned int pool_size_order = get_order(atomic_pool_size);
+
+	page = alloc_pages(GFP_DMA, pool_size_order);
+	if (!page && cma_available)
+		page = dma_alloc_from_contiguous(NULL, nr_pages,
+				pool_size_order);
+	if (page) {
+		int ret;
+		void *page_addr = page_address(page);
+
+		memset(page_addr, 0, atomic_pool_size);
+		__dma_flush_range(page_addr, page_addr + atomic_pool_size);
+
+		atomic_pool = gen_pool_create(PAGE_SHIFT, -1);
+		if (!atomic_pool)
+			goto free_page;
+
+		phys_addr = page_to_phys(page);
+		addr = (void *)__phys_to_coherent_virt(phys_addr);
+
+		if (!addr)
+			goto destroy_genpool;
+
+		ret = gen_pool_add_virt(atomic_pool, (unsigned long)addr,
+					page_to_phys(page),
+					atomic_pool_size, -1);
+		if (ret)
+			goto destroy_genpool;
+
+		gen_pool_set_algo(atomic_pool,
+				  gen_pool_first_fit_order_align,
+				  (void *)PAGE_SHIFT);
+
+		pr_info("DMA: preallocated %zu KiB pool for atomic allocations\n",
+			atomic_pool_size / 1024);
+		return 0;
+	}
+	goto out;
+
+destroy_genpool:
+	gen_pool_destroy(atomic_pool);
+	atomic_pool = NULL;
+free_page:
+	if (!dma_release_from_contiguous(NULL, page, nr_pages))
+		__free_pages(page, pool_size_order);
+out:
+	pr_err("DMA: failed to allocate %zu KiB pool for atomic coherent allocation\n",
+		atomic_pool_size / 1024);
+	return -ENOMEM;
+}
+
+static int __init arm64_dma_init(void)
+{
+	int ret = 0;
+
+	ret |= atomic_pool_init();
+
+	return ret;
+}
+arch_initcall(arm64_dma_init);
 
 #define PREALLOC_DMA_DEBUG_ENTRIES      4096
 
