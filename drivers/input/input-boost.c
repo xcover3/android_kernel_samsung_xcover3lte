2fea79641e047cefdb84dc685b6f5b711b45fb15
Fiona Yang <chy@marvell.com>
2014-12-03 14:11:03 +0800

    input: input-boost: add input_boost config interface to limit freq
    add interface to limit the minfreq and boost time when input boost
        inputbst_config/cpu_freq;  inputbst_config/cpu_time;
        inputbst_config/ddr_freq;  inputbst_config/ddr_time;
        inputbst_config/gc3d_freq; inputbst_config/gc3d_time;
        inputbst_config/gc2d_freq; inputbst_config/gc2d_time;
        inputbst_config/gcsh_freq; inputbst_config/gcsh_time;
        inputbst_config/vpu_freq; inputbst_config/vpu_time;
    when xx_time is 0, this xx component of boost is set to disable.
    
    Change-Id: I45e0ee22d716ca434542683f546fc2d8b56a462b
    Signed-off-by: Fiona Yang <chy@marvell.com>

--- drivers/input/input-boost.c
+++ drivers/input/input-boost.c
@@ -23,9 +23,40 @@
 #include <linux/pm_qos.h>
 #include <linux/slab.h>
 #include <linux/debugfs.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
 
 #define CPU_BOOST_TIME		200000
 #define MAX_FREQ		0x7FFFFFFF
+
+struct input_boost_config {
+	unsigned long boost_cpu_freq;
+	unsigned long boost_cpu_time;
+
+	unsigned long boost_ddr_freq;
+	unsigned long boost_ddr_time;
+
+	unsigned long boost_gc3d_freq;
+	unsigned long boost_gc3d_time;
+
+	unsigned long boost_gc2d_freq;
+	unsigned long boost_gc2d_time;
+
+	unsigned long boost_gcsh_freq;
+	unsigned long boost_gcsh_time;
+
+	unsigned long boost_vpu_freq;
+	unsigned long boost_vpu_time;
+};
+
+static struct input_boost_config boost_config = {
+	MAX_FREQ, CPU_BOOST_TIME,
+	MAX_FREQ, CPU_BOOST_TIME,
+	MAX_FREQ, CPU_BOOST_TIME,
+	MAX_FREQ, CPU_BOOST_TIME,
+	MAX_FREQ, CPU_BOOST_TIME,
+	MAX_FREQ, CPU_BOOST_TIME,
+};
 
 static struct pm_qos_request inputboost_cpu_qos_min = {
 	.name = "input_boost",
@@ -49,6 +80,12 @@ static struct pm_qos_request touchboost_gpush_qos_min = {
 	.name = "input_boost",
 };
 
+#ifdef CONFIG_VPU_DEVFREQ
+static struct pm_qos_request touchboost_vpu_qos_min = {
+	.name = "input_boost",
+};
+#endif
+
 static unsigned int boost_enabled;
 static struct work_struct inputboost_wk;
 
@@ -64,25 +101,38 @@ static struct work_struct inputboost_wk;
 static void inputboost_work(struct work_struct *w)
 {
 	/* boost cpu to max frequency 200ms by default */
-	pm_qos_update_request_timeout(&inputboost_cpu_qos_min,
-		MAX_FREQ, CPU_BOOST_TIME);
+	if (boost_config.boost_cpu_time)
+		pm_qos_update_request_timeout(&inputboost_cpu_qos_min,
+				boost_config.boost_cpu_freq, boost_config.boost_cpu_time);
 
 #ifdef CONFIG_DDR_DEVFREQ
-	/* boost ddr to max frequency */
-	pm_qos_update_request_timeout(&touchboost_ddr_qos_min,
-			MAX_FREQ, CPU_BOOST_TIME);
+	/* boost ddr to a limit frequency */
+	if (boost_config.boost_ddr_time)
+		pm_qos_update_request_timeout(&touchboost_ddr_qos_min,
+				boost_config.boost_ddr_freq, boost_config.boost_ddr_time);
 #endif
-	/* boost gpu0(3D) to max frequency */
-	pm_qos_update_request_timeout(&touchboost_gpu3d_qos_min,
-			MAX_FREQ, CPU_BOOST_TIME);
+	/* boost gpu0(3D) to a limit frequency */
+	if (boost_config.boost_gc3d_time)
+		pm_qos_update_request_timeout(&touchboost_gpu3d_qos_min,
+				boost_config.boost_gc3d_freq, boost_config.boost_gc3d_time);
 
-	/* boost gpu1(2D) to max frequency */
-	pm_qos_update_request_timeout(&touchboost_gpu2d_qos_min,
-			MAX_FREQ, CPU_BOOST_TIME);
+	/* boost gpu1(2D) to a limit frequency */
+	if (boost_config.boost_gc2d_time)
+		pm_qos_update_request_timeout(&touchboost_gpu2d_qos_min,
+				boost_config.boost_gc2d_freq, boost_config.boost_gc2d_time);
 
-	/* boost shader to max frequency */
-	pm_qos_update_request_timeout(&touchboost_gpush_qos_min,
-			MAX_FREQ, CPU_BOOST_TIME);
+	/* boost shader to a limit frequency */
+	if (boost_config.boost_gcsh_time)
+		pm_qos_update_request_timeout(&touchboost_gpush_qos_min,
+				boost_config.boost_gcsh_freq, boost_config.boost_gc3d_time);
+
+#ifdef CONFIG_VPU_DEVFREQ
+	/* boost vpu to a limit frequency */
+	if (boost_config.boost_vpu_time)
+		pm_qos_update_request_timeout(&touchboost_vpu_qos_min,
+				boost_config.boost_vpu_freq, boost_config.boost_vpu_time);
+#endif
+
 }
 
 static void keyboost_event(struct input_handle *handle, unsigned int type,
@@ -169,6 +219,64 @@ static const struct input_device_id keyboost_ids[] = {
 	}, /* keyboard */
 };
 
+#ifdef CONFIG_DEBUG_FS
+#define INPUT_BOOST_CONFIG_OPS(name)					\
+static ssize_t name##_read(struct file *file, char __user *user_buf,	\
+			       size_t count, loff_t *ppos)		\
+{									\
+	char val[20];					\
+	sprintf(val, "%d\n", (int)boost_config.boost_##name);		\
+	return simple_read_from_buffer(user_buf, count, ppos, val, strlen(val));	\
+}									\
+static ssize_t name##_write(struct file *filp,			\
+		const char __user *buffer, size_t count, loff_t *ppos)	\
+{									\
+	unsigned long data;						\
+	int ret = 0;							\
+	char buf[20] = { 0 };						\
+												\
+	if (count >= sizeof(buf))					\
+		return -EINVAL;							\
+	if (copy_from_user(buf, buffer, count))				\
+		return -EFAULT;						\
+											\
+	buf[count] = 0;							\
+	ret = kstrtoul(buf, 10, &data);			\
+	if (ret < 0) {							\
+		pr_err("%s: input <ERR> wrong parameter!\n", __func__);	\
+		return ret;					\
+	}									\
+	boost_config.boost_##name = data;				\
+									\
+	*ppos += count;					\
+	return count;					\
+}										\
+										\
+static const struct file_operations name##_ops = {			\
+	.read		= name##_read,					\
+	.write		= name##_write,				\
+};									\
+
+INPUT_BOOST_CONFIG_OPS(ddr_freq);
+INPUT_BOOST_CONFIG_OPS(ddr_time);
+
+INPUT_BOOST_CONFIG_OPS(cpu_freq);
+INPUT_BOOST_CONFIG_OPS(cpu_time);
+
+INPUT_BOOST_CONFIG_OPS(gc2d_freq);
+INPUT_BOOST_CONFIG_OPS(gc2d_time);
+
+INPUT_BOOST_CONFIG_OPS(gc3d_freq);
+INPUT_BOOST_CONFIG_OPS(gc3d_time);
+
+INPUT_BOOST_CONFIG_OPS(gcsh_freq);
+INPUT_BOOST_CONFIG_OPS(gcsh_time);
+
+INPUT_BOOST_CONFIG_OPS(vpu_freq);
+INPUT_BOOST_CONFIG_OPS(vpu_time);
+
+#endif
+
 static struct input_handler touchboost_handler = {
 	.event =	touchboost_event,
 	.connect =	touchboost_connect,
@@ -187,7 +295,15 @@ static struct input_handler keyboost_handler = {
 
 static int __init boost_init(void)
 {
-	int ret;
+	int ret1, ret2;
+	struct dentry *boost_node = NULL;
+	struct dentry *cpu_freq = NULL, *cpu_time = NULL;
+	struct dentry *ddr_freq = NULL, *ddr_time = NULL;
+	struct dentry *gc2d_freq = NULL, *gc2d_time = NULL;
+	struct dentry *gc3d_freq = NULL, *gc3d_time = NULL;
+	struct dentry *gcsh_freq = NULL, *gcsh_time = NULL;
+	struct dentry *vpu_freq = NULL, *vpu_time = NULL;
+
 	pm_qos_add_request(&inputboost_cpu_qos_min,
 		PM_QOS_CPUFREQ_MIN, PM_QOS_DEFAULT_VALUE);
 
@@ -204,15 +320,125 @@ static int __init boost_init(void)
 	pm_qos_add_request(&touchboost_gpush_qos_min,
 		PM_QOS_GPUFREQ_SH_MIN, PM_QOS_DEFAULT_VALUE);
 
+#ifdef CONFIG_VPU_DEVFREQ
+	pm_qos_add_request(&touchboost_vpu_qos_min,
+		PM_QOS_VPU_DEVFREQ_MIN, PM_QOS_DEFAULT_VALUE);
+#endif
+
 	INIT_WORK(&inputboost_wk, inputboost_work);
+
+	ret1 = input_register_handler(&touchboost_handler);
+	if (ret1)
+		pr_err("touchboost_handler register failed!\n");
+	ret2 = input_register_handler(&keyboost_handler);
+	if (ret2)
+		pr_err("keyboost_handler register failed\n");
+
+	if (ret1 && ret2)
+		return -ENOENT;
+
 #ifdef CONFIG_DEBUG_FS
 	debugfs_create_u32("inputbst_enable", 0644, NULL,
 		&boost_enabled);
+
+	boost_node = debugfs_create_dir("inputbst_config", NULL);
+	if (!boost_node) {
+		pr_err("debugfs inputbst_config created failed in %s\n", __func__);
+		return -ENOENT;
+	}
+
+	cpu_freq = debugfs_create_file("cpu_freq",
+			S_IRUGO | S_IWUSR | S_IWGRP, boost_node, NULL, &cpu_freq_ops);
+	if (!cpu_freq)
+		goto err_cpu_freq;
+
+	ddr_freq = debugfs_create_file("ddr_freq",
+			S_IRUGO | S_IWUSR | S_IWGRP, boost_node, NULL, &ddr_freq_ops);
+	if (!ddr_freq)
+		goto err_ddr_freq;
+
+	gc2d_freq = debugfs_create_file("gc2d_freq",
+			S_IRUGO | S_IWUSR | S_IWGRP, boost_node, NULL, &gc2d_freq_ops);
+	if (!gc2d_freq)
+		goto err_gc2d_freq;
+
+	gc3d_freq = debugfs_create_file("gc3d_freq",
+			S_IRUGO | S_IWUSR | S_IWGRP, boost_node, NULL, &gc3d_freq_ops);
+	if (!gc3d_freq)
+		goto err_gc3d_freq;
+
+	gcsh_freq = debugfs_create_file("gcsh_freq",
+			S_IRUGO | S_IWUSR | S_IWGRP, boost_node, NULL, &gcsh_freq_ops);
+	if (!gcsh_freq)
+		goto err_gcsh_freq;
+
+	vpu_freq = debugfs_create_file("vpu_freq",
+			S_IRUGO | S_IWUSR | S_IWGRP, boost_node, NULL, &vpu_freq_ops);
+	if (!vpu_freq)
+		goto err_vpu_freq;
+
+	cpu_time = debugfs_create_file("cpu_time",
+			S_IRUGO | S_IWUSR | S_IWGRP, boost_node, NULL, &cpu_time_ops);
+	if (!cpu_time)
+		goto err_cpu_time;
+
+	ddr_time = debugfs_create_file("ddr_time",
+			S_IRUGO | S_IWUSR | S_IWGRP, boost_node, NULL, &ddr_time_ops);
+	if (!ddr_time)
+		goto err_ddr_time;
+
+	gc2d_time = debugfs_create_file("gc2d_time",
+			S_IRUGO | S_IWUSR | S_IWGRP, boost_node, NULL, &gc2d_time_ops);
+	if (!gc2d_time)
+		goto err_gc2d_time;
+
+	gc3d_time = debugfs_create_file("gc3d_time",
+			S_IRUGO | S_IWUSR | S_IWGRP, boost_node, NULL, &gc3d_time_ops);
+	if (!gc3d_time)
+		goto err_gc3d_time;
+
+	gcsh_time = debugfs_create_file("gcsh_time",
+			S_IRUGO | S_IWUSR | S_IWGRP, boost_node, NULL, &gcsh_time_ops);
+	if (!gcsh_time)
+		goto err_gcsh_time;
+
+	vpu_time = debugfs_create_file("vpu_time",
+			S_IRUGO | S_IWUSR | S_IWGRP, boost_node, NULL, &vpu_time_ops);
+	if (!vpu_time)
+		goto err_vpu_time;
 #endif
-	ret = input_register_handler(&touchboost_handler);
-	if (ret)
-		pr_err("inputboost_handler register failed");
-	return input_register_handler(&keyboost_handler);
+
+	return 0;
+
+#ifdef CONFIG_DEBUG_FS
+err_vpu_time:
+	debugfs_remove(gcsh_time);
+err_gcsh_time:
+	debugfs_remove(gc3d_time);
+err_gc3d_time:
+	debugfs_remove(gc2d_time);
+err_gc2d_time:
+	debugfs_remove(ddr_time);
+err_ddr_time:
+	debugfs_remove(cpu_time);
+err_cpu_time:
+	debugfs_remove(vpu_freq);
+err_vpu_freq:
+	debugfs_remove(gcsh_freq);
+err_gcsh_freq:
+	debugfs_remove(gc3d_freq);
+err_gc3d_freq:
+	debugfs_remove(gc2d_freq);
+err_gc2d_freq:
+	debugfs_remove(ddr_freq);
+err_ddr_freq:
+	debugfs_remove(cpu_freq);
+err_cpu_freq:
+	debugfs_remove(boost_node);
+	pr_err("debugfs entry created failed in %s\n", __func__);
+	return -ENOENT;
+#endif
+
 }
 
 static void __exit boost_exit(void)
