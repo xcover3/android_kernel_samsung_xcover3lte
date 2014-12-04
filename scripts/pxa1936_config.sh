7e197aad49ba93d34fe2d381d61faf499c273a88
Xiaoguang Chen <chenxg@marvell.com>
2014-12-04 13:18:13 +0800

    scripts: pxa1936: enable cpuidle on helan3
    Change-Id: Ia04894844dccf1c16691f047ed636df5f772f403
    Signed-off-by: Xiaoguang Chen <chenxg@marvell.com>

--- scripts/pxa1936_config.sh
+++ scripts/pxa1936_config.sh
@@ -6,8 +6,6 @@
 ./scripts/config --set-val CONFIG_NR_CPUS 8
 ./scripts/config -d CONFIG_NO_HOTPLUG_POLICY
 ./scripts/config -d CPU_HOTPLUG_POL_STANDALONE
-./scripts/config -d CONFIG_CPU_IDLE
-./scripts/config -d CONFIG_CPU_IDLE_MMP_V8
 ./scripts/config -e CONFIG_ARM_MMP_BL_CPUFREQ
 ./scripts/config -d CONFIG_ARM_MMP_SMP_CPUFREQ
 ./scripts/config -d CONFIG_PXA1928_THERMAL
