4899f191286e14b2cc4be38ec96471c7573b288b
Yi Zhang <yizhang@marvell.com>
2014-12-08 20:17:30 +0800

    mfd: 88pm88x-dvc: add 88pm880 support
    - abstract a set of operation functions for chip level private callback
    - support 88pm880, which has 16 levels at most
    
    Change-Id: If6197f392b9b82870fabd9e0434531af3e4ae94a
    Signed-off-by: Yi Zhang <yizhang@marvell.com>

--- drivers/mfd/88pm88x-dvc.c
+++ drivers/mfd/88pm88x-dvc.c
@@ -1,5 +1,5 @@
 /*
- * DVC (dynamic voltage change) driver for Marvell 88PM886
+ * DVC (dynamic voltage change) driver for Marvell 88PM88x
  * - Derived from dvc driver for 88pm800/88pm822/88pm860
  *
  * Copyright (C) 2014 Marvell International Ltd.
@@ -19,6 +19,7 @@
 #include <linux/regulator/machine.h>
 #include <linux/mfd/88pm88x.h>
 #include <linux/mfd/88pm886.h>
+#include <linux/mfd/88pm880.h>
 #include <linux/delay.h>
 #include <linux/io.h>
 #include <linux/of.h>
@@ -29,23 +30,68 @@
 #define BUCK_MIN_VOLT			(600000)
 #define BUCK_MAX_VOLT			(1587500)
 #define BUCK_STEP			(12500)
-#define BUCK_MAX_DVC_LEVEL		(8)
 
-/* encapsulate for future usage */
-struct pm88x_dvc {
-	struct pm88x_chip *chip;
+struct pm88x_dvc_ops {
+	void (*level_to_reg)(u8 level);
 };
 
+struct pm88x_buck1_dvc_desc {
+	u8 current_reg;
+	int max_level;
+	int uV_step1;
+	int uV_step2;
+	int min_uV;
+	int mid_uV;
+	int max_uV;
+	int mid_reg_val;
+};
+
+struct pm88x_dvc {
+	struct device *dev;
+	struct pm88x_chip *chip;
+	struct pm88x_dvc_ops ops;
+	struct pm88x_buck1_dvc_desc desc;
+};
 static struct pm88x_dvc *g_dvc;
 
-static inline int volt_to_reg(int uv)
+static inline int map_volt_to_reg(int uv)
 {
-	return (uv - BUCK_MIN_VOLT) / BUCK_STEP;
+	if (uv >= g_dvc->desc.mid_uV)
+		return (uv - g_dvc->desc.mid_uV) / (g_dvc->desc.uV_step2);
+	else
+		return (uv - g_dvc->desc.min_uV) / (g_dvc->desc.uV_step1);
 }
 
-static inline int reg_to_volt(int regval)
+static inline int list_volt_from_reg(int reg_val)
 {
-	return regval * BUCK_STEP + BUCK_MIN_VOLT;
+	if (reg_val >= g_dvc->desc.mid_reg_val)
+		return reg_val * (g_dvc->desc.uV_step2) + g_dvc->desc.mid_uV;
+	else
+		return reg_val * (g_dvc->desc.uV_step1) + g_dvc->desc.min_uV;
+}
+
+static void pm886_level_to_reg(u8 level)
+{
+	if (level < 0 || level >= g_dvc->desc.max_level) {
+		dev_err(g_dvc->dev, "DVC level is out of range!\n");
+		return;
+	}
+
+	if (level < 4)
+		g_dvc->desc.current_reg = PM886_BUCK1_VOUT + level;
+
+	else
+		g_dvc->desc.current_reg = PM886_BUCK1_4_VOUT + level - 4;
+}
+
+static void pm880_level_to_reg(u8 level)
+{
+	if (level < 0 || level >= g_dvc->desc.max_level) {
+		dev_err(g_dvc->dev, "DVC level is out of range!\n");
+		return;
+	}
+
+	g_dvc->desc.current_reg = PM880_BUCK1_VOUT + level;
 }
 
 /*
@@ -55,7 +101,6 @@ static inline int reg_to_volt(int regval)
  */
 int pm88x_dvc_set_volt(u8 level, int uv)
 {
-	u8 buck1_volt_reg;
 	int ret = 0;
 	struct regmap *regmap;
 
@@ -65,66 +110,99 @@ int pm88x_dvc_set_volt(u8 level, int uv)
 	}
 	regmap = g_dvc->chip->buck_regmap;
 
-	if (uv < BUCK_MIN_VOLT || uv > BUCK_MAX_VOLT) {
-		dev_err(g_dvc->chip->dev, "the expected voltage is out of range!\n");
+	if (uv < g_dvc->desc.min_uV || uv > g_dvc->desc.max_uV) {
+		dev_err(g_dvc->dev, "the expected voltage is out of range!\n");
 		return -EINVAL;
 	}
 
-	if (level < 0 || level >= BUCK_MAX_DVC_LEVEL) {
-		dev_err(g_dvc->chip->dev, "DVC level is out of range!\n");
-		return -EINVAL;
-	}
+	g_dvc->ops.level_to_reg(level);
 
-	if (level < 4)
-		buck1_volt_reg = PM886_BUCK1_VOUT;
-	else {
-		buck1_volt_reg = PM886_BUCK1_4_VOUT;
-		level -= 4;
-	}
-
-	/* pay attention to only change the voltage value */
-	ret = regmap_update_bits(regmap, buck1_volt_reg + level,
-				 0x7f, volt_to_reg(uv));
+	ret = regmap_update_bits(regmap, g_dvc->desc.current_reg, 0x7f,
+				 map_volt_to_reg(uv));
 	return ret;
 };
-EXPORT_SYMBOL(pm88x_dvc_set_volt);
 
 int pm88x_dvc_get_volt(u8 level)
 {
 	struct regmap *regmap;
 	int ret = 0, regval = 0;
-	u8 buck1_volt_reg;
+	int volt;
 
 	if (!g_dvc || !g_dvc->chip || !g_dvc->chip->buck_regmap) {
 		pr_err("%s: NULL pointer!\n", __func__);
 		return -EINVAL;
 	}
 
+	g_dvc->ops.level_to_reg(level);
+
 	regmap = g_dvc->chip->buck_regmap;
-	if (level >= BUCK_MAX_DVC_LEVEL) {
-		dev_err(g_dvc->chip->dev, "%s: DVC level out of range\n", __func__);
-		return -EINVAL;
-	}
-
-	if (level < 4)
-		buck1_volt_reg = PM886_BUCK1_VOUT;
-	else {
-		buck1_volt_reg = PM886_BUCK1_4_VOUT;
-		level -= 4;
-	}
-
-	ret = regmap_read(regmap, buck1_volt_reg + level, &regval);
+	ret = regmap_read(regmap, g_dvc->desc.current_reg, &regval);
 	if (ret < 0) {
-		dev_err(g_dvc->chip->dev,
-			"fail to read reg: 0x%x\n", buck1_volt_reg + level);
+		dev_err(g_dvc->dev, "fail to read reg: 0x%x\n",
+			g_dvc->desc.current_reg);
+		return ret;
+	}
+	regval &= 0x7f;
+
+	volt = list_volt_from_reg(regval);
+	if (volt < g_dvc->desc.min_uV || volt > g_dvc->desc.max_uV) {
+		dev_err(g_dvc->dev,
+			"voltage out of range: %duV, level = %d\n", volt, level);
+		return g_dvc->desc.min_uV;
+	}
+
+	return volt;
+}
+
+/* configure according to chip type */
+static int pm88x_dvc_chip_init(struct pm88x_dvc *dvc)
+{
+	int ret = -EINVAL;
+
+	if (!dvc || !dvc->chip)
+		return -EINVAL;
+
+	switch (dvc->chip->type) {
+	case PM886:
+		/* config gpio1 as DVC3 pin for 88pm886 */
+		ret = regmap_update_bits(dvc->chip->base_regmap,
+					 PM88X_GPIO_CTRL1,
+					 PM88X_GPIO1_MODE_MSK,
+					 PM88X_GPIO1_SET_DVC);
+		if (ret < 0) {
+			dev_err(dvc->dev, "Failed to set gpio1 as dvc pin!\n");
+			return ret;
+		}
+		dvc->desc.current_reg = PM886_BUCK1_VOUT;
+		dvc->desc.mid_reg_val = 0x50;
+		dvc->desc.max_level = 8;
+		dvc->desc.uV_step1 = 12500;
+		dvc->desc.uV_step2 = 50000;
+		dvc->desc.min_uV = 600000;
+		dvc->desc.mid_uV = 1600000;
+		dvc->desc.max_uV = 1800000;
+
+		dvc->ops.level_to_reg = pm886_level_to_reg;
+		break;
+	case PM880:
+		/* TODO: configure gpio to dvc function for 88pm880 */
+		dvc->desc.current_reg = PM886_BUCK1_VOUT;
+		dvc->desc.mid_reg_val = 0x50;
+		dvc->desc.max_level = 16;
+		dvc->desc.uV_step1 = 12500;
+		dvc->desc.uV_step2 = 50000;
+		dvc->desc.min_uV = 600000;
+		dvc->desc.mid_uV = 1600000;
+		dvc->desc.max_uV = 1800000;
+
+		dvc->ops.level_to_reg = pm880_level_to_reg;
+		break;
+	default:
 		return ret;
 	}
 
-	regval &= 0x7f;
-
-	return reg_to_volt(regval);
+	return 0;
 }
-EXPORT_SYMBOL(pm88x_dvc_get_volt);
 
 static int pm88x_dvc_probe(struct platform_device *pdev)
 {
@@ -139,15 +217,15 @@ static int pm88x_dvc_probe(struct platform_device *pdev)
 	}
 
 	platform_set_drvdata(pdev, dvcdata);
+
 	/* get global handler */
 	g_dvc = dvcdata;
 	g_dvc->chip = chip;
+	g_dvc->dev = &pdev->dev;
 
-	/* config gpio1 as DVC3 pin */
-	ret = regmap_update_bits(chip->base_regmap, PM88X_GPIO_CTRL1,
-				 PM88X_GPIO1_MODE_MSK, PM88X_GPIO1_SET_DVC);
+	ret = pm88x_dvc_chip_init(g_dvc);
 	if (ret < 0) {
-		dev_err(&pdev->dev, "Failed to set gpio1 as dvc pin!\n");
+		dev_err(&pdev->dev, "Failed to do chip specific dvc setting!\n");
 		return ret;
 	}
 
@@ -170,7 +248,7 @@ static int pm88x_dvc_remove(struct platform_device *pdev)
 }
 
 static struct of_device_id pm88x_dvc_of_match[] = {
-	{.compatible = "marvell,88pm886-dvc"},
+	{.compatible = "marvell,88pm88x-dvc"},
 	{},
 };
 
@@ -178,7 +256,7 @@ MODULE_DEVICE_TABLE(of, pm88x_dvc_of_match);
 
 static struct platform_driver pm88x_dvc_driver = {
 	.driver = {
-		   .name = "88pm886-dvc",
+		   .name = "88pm88x-dvc",
 		   .owner = THIS_MODULE,
 		   .of_match_table = of_match_ptr(pm88x_dvc_of_match),
 		   },
@@ -200,4 +278,4 @@ module_exit(pm88x_dvc_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("DVC Driver for Marvell 88PM886 PMIC");
-MODULE_ALIAS("platform:88pm886-dvc");
+MODULE_ALIAS("platform:88pm88x-dvc");
