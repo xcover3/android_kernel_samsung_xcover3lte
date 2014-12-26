794c105172249d7e394935095b9fc95c0583ec7b
Nan Xiang <nanxiang@marvell.com>
2014-12-26 16:06:19 +0800

    usb: gadget: port_usb may be NULL after gs_start_rx
    CQ00079627 Kernel Panic at gs_start_io after adb reboot for 1312 loops.
    logs:
    [   16.689428] c0 2309 (adbd) mv-udc mv-udc: complete ep4out req ffffffc03a62c180 stat -104 len 24/24
    [   16.699571] c0 2309 (adbd) adb_release
    [   16.703793] c1 2314 (modemDevRx) mv-udc mv-udc: udc or ep2out is already disabled!
    [   16.711393] c1 2314 (modemDevRx) Unable to handle kernel NULL pointer dereference at virtual address 000000b8
    [   16.721327] c1 2314 (modemDevRx) pgd = ffffffc038483000
    [   16.726549] [000000b8] *pgd=0000000000000000
    [   16.730823] c1 2314 (modemDevRx) Internal error: Oops: 96000005 [#1] PREEMPT SMP
    [   16.738217] Modules linked in: tzdd galcore(O)
    [   16.742685] c1 2314 (modemDevRx) CPU: 1 PID: 2314 Comm: modemDevRx Tainted: G           O 3.14.26 #1
    [   16.751814] c1 2314 (modemDevRx) task: ffffffc03a575e00 ti: ffffffc038774000 task.ti: ffffffc038774000
    [   16.761137] c1 2314 (modemDevRx) PC is at gs_start_io+0xe0/0x13c
    [   16.767174] c1 2314 (modemDevRx) LR is at gs_start_io+0xd8/0x13c
    [   16.767180] c1 2314 (modemDevRx) pc : [<ffffffc00048d1fc>] lr : [<ffffffc00048d1f4>] pstate: 600001c5
    [   16.767185] c1 2314 (modemDevRx) sp : ffffffc038777a60
    [   16.767194] c1 2314 (modemDevRx) x29: ffffffc038777a60 x28: ffffffc0310b2800
    [   16.767201] c1 2314 (modemDevRx) x27: 0000000000020002 x26: ffffffc002bcb9e8
    [   16.767208] c1 2314 (modemDevRx) x25: ffffffc002bcb9fc x24: 0000000000000000
    [   16.767215] c1 2314 (modemDevRx) x23: ffffffc03c7971f8 x22: ffffffc002bcb990
    [   16.767222] c1 2314 (modemDevRx) x21: ffffffc002bcb9a4 x20: 0000000000000000
    [   16.767229] c1 2314 (modemDevRx) x19: ffffffc002bcb800 x18: 0000000000000000
    [   16.767236] c1 2314 (modemDevRx) x17: 0000000000000000 x16: ffffffc0001f25b0
    [   16.767242] c1 2314 (modemDevRx) x15: 0000000000000000 x14: ffffffc000c8e000
    
    gs_start_rx drop port_lock when it calls out using usb_ep_queue,
    the controller driver may need to call gserial_disconnect (usually from irq),
    in gserial_disconnect the port->port_usb will be set to NULL, when
    the gs_start_rx returns, the port->port_usb is NULL, the call
    gs_free_requests(port->port_usb->in, &port->write_pool,....)
    will cause a KERNEL PANIC!
    
    use an auto variable gser to store port->port_usb to avoid accessing
    port->port_usb directly.
    
    Change-Id: Icac755cf4d5e70af9b21fe30b201d51973edfd12
    Signed-off-by: Nan Xiang <nanxiang@marvell.com>

--- drivers/usb/gadget/u_serial.c
+++ drivers/usb/gadget/u_serial.c
@@ -671,6 +671,7 @@ static int gs_start_io(struct gs_port *port)
 {
 	struct list_head	*head = &port->read_pool;
 	struct usb_ep		*ep = port->port_usb->out;
+	struct gserial		*gser = port->port_usb;
 	int			status;
 	unsigned		started;
 
@@ -685,7 +686,7 @@ static int gs_start_io(struct gs_port *port)
 	if (status)
 		return status;
 
-	status = gs_alloc_requests(port->port_usb->in, &port->write_pool,
+	status = gs_alloc_requests(gser->in, &port->write_pool,
 			gs_write_complete, &port->write_allocated);
 	if (status) {
 		gs_free_requests(ep, head, &port->read_allocated);
@@ -701,7 +702,7 @@ static int gs_start_io(struct gs_port *port)
 		tty_wakeup(port->port.tty);
 	} else {
 		gs_free_requests(ep, head, &port->read_allocated);
-		gs_free_requests(port->port_usb->in, &port->write_pool,
+		gs_free_requests(gser->in, &port->write_pool,
 			&port->write_allocated);
 		status = -EIO;
 	}
