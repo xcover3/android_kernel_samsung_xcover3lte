90269a30cb7dd494c3b9a189353e5163e4a7ad53
Fangsuo Wu <fswu@marvell.com>
2014-12-30 19:23:06 +0800

    drivers: debug: Enable interrupt mode for axi timeout
    Current axi timeout only supports data abort mode. But if cpu accesses
    another AXI s2 port address before AXI timeout response returns, the
    cpu will hang immediately and has no chance to jump to data abort
    handler.
    
    To elimate such limitation, this patch enables axi timeout interrupt
    mode. It routes axi timeout interrupts to all AP CPUs and brings
    benefits as below:
    1. If cpu0 hangs due to axi timeout, the interrupt can be handlered
    by any one of the other cpus;
    2. If axi timeout is caused by CM3 or CP, AP can help to reserve the
    axi timeout address.
    
    Change-Id: I823bdeeca3f0d87d5f6a59f0720d513f54c0a55f
    Signed-off-by: Fangsuo Wu <fswu@marvell.com>

--- drivers/marvell/debug/mmp-debug.c
+++ drivers/marvell/debug/mmp-debug.c
@@ -9,21 +9,24 @@
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  */
-
 #include <linux/io.h>
+#include <linux/module.h>
 #include <linux/of.h>
 #include <linux/init.h>
+#include <linux/interrupt.h>
 #include <linux/reboot.h>
 #include <linux/kexec.h>
 #include <linux/signal.h>
 #include <linux/of_address.h>
+#include <linux/platform_device.h>
 
 #include <asm/cacheflush.h>
 #include <asm/system_misc.h>
 
-#define FAB_TIMEOUT_CTRL	0x60
-#define FAB_TIMEOUT_STATUS0	0x68
-#define FAB_TIMEOUT_STATUS1	0x70
+#define DRVNAME				"axi timeout"
+#define FAB_TIMEOUT_CTRL		0x60
+#define FAB_TIMEOUT_STATUS0		0x68
+#define FAB_TIMEOUT_STATUS1		0x70
 
 #define STATE_HOLD_CTRL		0x88
 #define FABTIMEOUT_HELD_WSTATUS	0x78
@@ -39,6 +42,9 @@
 #define PMULPM_HELD_STATUS_V2	0xC8
 #define CORELPM_HELD_STATUS_V2	0x100
 #define PLL_HELD_STATUS_V2	0x108
+
+#define INT_MODE			0x1
+#define DATA_ABORT_MODE			0x2
 
 struct held_status {
 	u32 fabws;
@@ -68,20 +74,20 @@ static unsigned long err_addr;
 
 static struct held_status recorded_helds;
 
-static int mmp_axi_timeout(unsigned long addr, unsigned int fsr,
-				struct pt_regs *regs)
+static int axi_timeout_check(void)
 {
-	struct pt_regs fixed_regs;
-
 	timeout_write_addr = readl_relaxed(squ_base + FAB_TIMEOUT_STATUS0);
 	timeout_read_addr = readl_relaxed(squ_base + FAB_TIMEOUT_STATUS1);
 
-	/* Return For those not caused by AXI timeout */
-	if (!(timeout_write_addr & 0x1)	&& !(timeout_read_addr & 0x1))
-		return 1;
+	if ((timeout_write_addr & 0x1) || (timeout_read_addr & 0x1))
+		return 0;
 
-	err_fsr = fsr;
-	err_addr = addr;
+	return 1;
+}
+
+static void setup_crash_info(struct pt_regs *regs)
+{
+	struct pt_regs fixed_regs;
 
 	set_emmd_indicator();
 
@@ -96,9 +102,32 @@ static int mmp_axi_timeout(unsigned long addr, unsigned int fsr,
 	/* Waiting wdt to reset the Soc */
 	while (1)
 		;
+}
+
+static int axi_timeout_data_abort_handler(unsigned long addr, unsigned int fsr,
+						struct pt_regs *regs)
+{
+	if (axi_timeout_check())
+		return 0;
+
+	err_fsr = fsr;
+	err_addr = addr;
+
+	setup_crash_info(regs);
 
 	return 0;
 }
+
+static irqreturn_t axi_timeout_int_handler(int irq, void *dev_id)
+{
+	if (axi_timeout_check())
+		return IRQ_HANDLED;
+
+	setup_crash_info(NULL);
+
+	return IRQ_HANDLED;
+}
+
 
 /* dump Fabric/LPM/DFC/DVC held status and enable held feature */
 static int __init mmp_dump_heldstatus(void __iomem *squ_base)
@@ -157,58 +186,123 @@ static int __init mmp_dump_heldstatus(void __iomem *squ_base)
 	return 0;
 }
 
-static int __init mmp_debug_init(void)
-{
-	struct device_node *node;
-	int ret;
-	u32 tmp;
+static const struct of_device_id axi_timeout_dt_match[] = {
+	{.compatible = "marvell,mmp-debug"},
+	{},
+};
 
-	node = of_find_compatible_node(NULL, NULL, "marvell,mmp-debug");
-	if (!node) {
-		pr_info("This platform doesn't support debug feature!\n");
-		return -ENODEV;
+MODULE_DEVICE_TABLE(of, axi_timeout_dt_match);
+
+static int __ref axi_timeout_probe(struct platform_device *pdev)
+{
+	u32 tmp, modes;
+	int irq, i, ret = 0;
+	struct resource *res;
+	struct cpumask mask_val;
+	struct device_node *np = pdev->dev.of_node;
+
+	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
+	if (res == NULL) {
+		dev_err(&pdev->dev, "%s: no IO memory defined\n", __func__);
+		ret = -ENOENT;
+		goto failed;
 	}
 
-	ret = of_property_read_u32(node, "version", &version);
+	irq = platform_get_irq(pdev, 0);
+	if (irq < 0)
+		dev_warn(&pdev->dev, "%s: no IRQ defined\n", __func__);
+
+	if (!devm_request_mem_region(&pdev->dev, res->start,
+			resource_size(res), DRVNAME)) {
+		dev_err(&pdev->dev, "can't request region for resource %pR\n", res);
+		ret = -EINVAL;
+		goto failed;
+	}
+
+	squ_base = devm_ioremap(&pdev->dev, res->start, resource_size(res));
+	if (squ_base == NULL) {
+		dev_err(&pdev->dev, "%s: res %lx - %lx map failed\n", __func__,
+				(unsigned long)res->start, (unsigned long)res->end);
+		ret = -ENOMEM;
+		goto failed;
+	}
+
+	ret = of_property_read_u32(np, "version", &version);
 	if (ret < 0) {
 		pr_err("get version failed, use version 1\n");
 		version = MMP_DEBUG_VERSION_1;
 	}
 
-	squ_base = of_iomap(node, 0);
-	if (!squ_base) {
-		pr_err("Failed to map squ register\n");
-		return -ENOMEM;
+	if (of_property_read_u32(np, "detection_modes", &modes)) {
+		ret = -EINVAL;
+		dev_err(&pdev->dev, "%s: detection modes not defined!\n", __func__);
+		goto failed;
 	}
 
-	/* configure to data abort mode */
 	tmp = readl_relaxed(squ_base + FAB_TIMEOUT_CTRL);
-	tmp |= (1 << 29) | (1 << 30);
+	tmp |= (1 << 29 | 1 << 30 | 1 << 31);
 	writel_relaxed(tmp, squ_base + FAB_TIMEOUT_CTRL);
 
-	/* Register debug fault handler. */
+	if (modes & INT_MODE) {
+		tmp = readl_relaxed(squ_base + FAB_TIMEOUT_CTRL);
+		tmp &= ~(1 << 29);
+		writel_relaxed(tmp, squ_base + FAB_TIMEOUT_CTRL);
+
+		ret = devm_request_irq(&pdev->dev, irq, axi_timeout_int_handler,
+				IRQF_TRIGGER_RISING | IRQF_MULTI_CPUS, "axi_timeout", NULL);
+		if (ret) {
+			dev_err(&pdev->dev, "%s: Failed to request irq!\n", __func__);
+			goto failed;
+		}
+
+		/* Route interrupt to ALL cpus */
+		for (i = 0; i < nr_cpu_ids; i++)
+			cpumask_set_cpu(i, &mask_val);
+		irq_set_affinity(irq, &mask_val);
+	}
+
+	if (modes & DATA_ABORT_MODE) {
+		tmp = readl_relaxed(squ_base + FAB_TIMEOUT_CTRL);
+		tmp &= ~(1 << 31);
+		writel_relaxed(tmp, squ_base + FAB_TIMEOUT_CTRL);
+
 #ifdef CONFIG_ARM
-	hook_fault_code(0x8, mmp_axi_timeout, SIGBUS,
-			0, "external abort on non-linefetch");
-	hook_fault_code(0xc, mmp_axi_timeout, SIGBUS,
-			0, "external abort on translation");
-	hook_fault_code(0xe, mmp_axi_timeout, SIGBUS,
-			0, "external abort on translation");
-	hook_fault_code(0x10, mmp_axi_timeout, SIGBUS,
-			0, "unknown 16");
-	hook_fault_code(0x16, mmp_axi_timeout, SIGBUS,
-			BUS_OBJERR, "imprecise external abort");
+		hook_fault_code(0x8, axi_timeout_data_abort_handler, SIGBUS,
+				0, "external abort on non-linefetch");
+		hook_fault_code(0xc, axi_timeout_data_abort_handler, SIGBUS,
+				0, "external abort on translation");
+		hook_fault_code(0xe, axi_timeout_data_abort_handler, SIGBUS,
+				0, "external abort on translation");
+		hook_fault_code(0x10, axi_timeout_data_abort_handler, SIGBUS,
+				0, "unknown 16");
+		hook_fault_code(0x16, axi_timeout_data_abort_handler, SIGBUS,
+				BUS_OBJERR, "imprecise external abort");
 #endif
 
 #ifdef CONFIG_ARM64
-	hook_fault_code(0x10, mmp_axi_timeout, SIGBUS,
-			0, "synchronous external abort");
-	hook_fault_code(0x11, mmp_axi_timeout, SIGBUS,
-			0, "asynchronous external abort");
+		hook_fault_code(0x10, axi_timeout_data_abort_handler, SIGBUS,
+				0, "synchronous external abort");
+		hook_fault_code(0x11, axi_timeout_data_abort_handler, SIGBUS,
+				0, "asynchronous external abort");
 #endif
+	}
 
 	mmp_dump_heldstatus(squ_base);
-	return 0;
+
+failed:
+	return ret;
 }
 
-arch_initcall(mmp_debug_init);
+static struct platform_driver axi_timeout_driver = {
+	.probe = axi_timeout_probe,
+	.driver = {
+		.owner = THIS_MODULE,
+		.name = DRVNAME,
+		.of_match_table = of_match_ptr(axi_timeout_dt_match),
+	},
+};
+
+module_platform_driver(axi_timeout_driver);
+
+MODULE_DESCRIPTION("AXI TIMEOUT DRIVER");
+MODULE_LICENSE("GPL");
