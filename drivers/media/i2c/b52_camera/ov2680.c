6ca467b7f5d2e2f26d16c3e3b7898fa62205f558
Albert Guo <csguo@marvell.com>
2015-01-29 16:33:47 +0800

    media: i2c: b52_camera: modify ov2680 sensor setting
    Porting the sensor setting from 1928_beta2 branch that has be validated
    
    Change-Id: I74b0846b844ffe8884f374c36e2995b600fcd950
    Signed-off-by: Albert Guo <csguo@marvell.com>

--- drivers/media/i2c/b52_camera/ov2680.c
+++ drivers/media/i2c/b52_camera/ov2680.c
@@ -28,9 +28,48 @@
 #include <linux/of_device.h>
 #include "ov2680.h"
 
+static int OV2680_get_mipiclock(struct v4l2_subdev *sd, u32 *rate, u32 mclk)
+{
+	int temp1, temp2;
+	int pre_div0, pre_div2x, div_loop, VCO, sp_div, lane_div;
+	int pre_div2x_map[] = {2, 3, 4, 5, 6, 8, 12, 16};
+
+	struct b52_sensor *sensor = to_b52_sensor(sd);
+
+	b52_sensor_call(sensor, i2c_read, 0x3088, &temp1, 1);
+	if (temp1 & 0x10)
+		pre_div0 = 2;
+	else
+		pre_div0 = 1;
+
+	b52_sensor_call(sensor, i2c_read, 0x3080, &temp1, 1);
+	temp2 = temp1 & 0x07;
+	pre_div2x = pre_div2x_map[temp2];
+
+	b52_sensor_call(sensor, i2c_read, 0x3081, &temp1, 1);
+	temp2 = ((temp1 & 0x03) << 2);
+	b52_sensor_call(sensor, i2c_read, 0x3082, &temp1, 1);
+	div_loop = temp1 + temp2;
+
+	VCO = mclk * 2 / pre_div0 / pre_div2x * div_loop;
+
+	b52_sensor_call(sensor, i2c_read, 0x3086, &temp1, 1);
+	sp_div = (temp1 & 0x0f) + 1;
+	b52_sensor_call(sensor, i2c_read, 0x3087, &temp1, 1);
+	lane_div = 1 << (temp1 & 0x03);
+
+	*rate = VCO / sp_div / lane_div;
+
+	return 0;
+}
+
 static int OV2680_get_dphy_desc(struct v4l2_subdev *sd,
 			struct csi_dphy_desc *dphy_desc, u32 mclk)
 {
+	OV2680_get_mipiclock(sd, &dphy_desc->clk_freq, mclk);
+	dphy_desc->hs_prepare = 70;
+	dphy_desc->hs_zero  = 100;
+
 	return 0;
 }
 
@@ -56,7 +95,7 @@ static int OV2680_get_pixelclock(struct v4l2_subdev *sd, u32 *rate, u32 mclk)
 	b52_sensor_call(sensor, i2c_read, 0x3082, &temp1, 1);
 	div_loop = temp1 + temp2;
 
-	VCO = mclk * sensor->drvdata->nr_lane / pre_div0 / pre_div2x * div_loop;
+	VCO = mclk * 2 / pre_div0 / pre_div2x * div_loop;
 
 	b52_sensor_call(sensor, i2c_read, 0x3086, &temp1, 1);
 	sp_div = (temp1 & 0x0f) + 1;
