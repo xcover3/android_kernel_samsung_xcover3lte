494bf438d431dde9ab436811ea83f4ce1f5f5447
Johnson Lu <lllu@marvell.com>
2015-03-31 20:31:06 +0800

    PM / devfreq: create folder ddr_upthreshold under /sys/kernel/
    Used to expose DDR up threshold tuning parameter under this folder
    for power and performance tuning.
    
    Example:
    /sys/kernel/ddr_upthreshold/upthrd_usr
    /sys/kernel/ddr_upthreshold/upthrd_downdiff
    
    Change-Id: I43839bf2f36e6558db78484db0d11e68924b454c
    Signed-off-by: Johnson Lu <lllu@marvell.com>

---
+++ include/linux/ddr_upthreshold.h
@@ -0,0 +1,18 @@
+/*
+ * include/linux/ddr_upthreshold.h
+ *
+ * Author:	Johnson Lu <lllu@marvell.com>
+ * Copyright:	(C) 2015 Marvell International Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef _LINUX_DDR_UPTHRESHOLD_H
+#define _LINUX_DDR_UPTHRESHOLD_H
+
+extern struct kobject *ddr_upthrd_obj;
+
+#endif /* _LINUX_DDR_UPTHRESHOLD_H */
