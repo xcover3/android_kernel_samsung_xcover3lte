3cd641e679c0ec46e6afe04a0174ff047fbc3b95
Zhigang Hu <gavinhu@marvell.com>
2015-04-27 10:40:30 +0800

    mm: page_alloc: fix-access-NULL-bug
    In the extreme situation, there isn’t any page can be allocated from CMA. 
    Then sel_page will be NULL(NULL is its initial value). 
    So “set_freepage_migratetype(sel_page, MIGRATE_CMA)” will  result in
    trying to access NULL. ….
    
    The precondition “if (sel_page)” is necessary for 
    “set_freepage_migratetype(sel_page, MIGRATE_CMA)”.
    
    
    Change-Id: I2469feca7494183db7f4c37c157985f99d4f4f07
    Signed-off-by: Zhigang Hu <gavinhu@marvell.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1071,9 +1071,8 @@ struct page *__rmqueue_highest_cma(struct zone *zone, unsigned int order)
 		}
 
 		sel_page = &sel_page[offset];
+		set_freepage_migratetype(sel_page, MIGRATE_CMA);
 	}
-
-	set_freepage_migratetype(sel_page, MIGRATE_CMA);
 
 	return sel_page;
 }
