c79fefdcb1aa609764efd83c422b57abec9b702c
Tim Wang <wangtt@marvell.com>
2015-06-10 11:09:01 +0800

    Revert "misc: sd8x-rfkill: request edge wakeup pin once"
    Revert this patch since the ATF code add remove edge request feature.
    
    This reverts commit 7a6f8365b839908e37f08762b21878d50cf2153d.
    
    Change-Id: I0015e505bfd12a94697bcdc89a4c082a0c9f1d7b
    Signed-off-by: Tim Wang <wangtt@marvell.com>

--- drivers/misc/sd8x_rfkill.c
+++ drivers/misc/sd8x_rfkill.c
@@ -367,12 +367,9 @@ static int sd8x_pwr_on(struct sd8x_rfkill_platform_data *pdata)
 		pdata->is_on = 1;
 
 		if (pdata->gpio_edge_wakeup >= 0) {
-			if (!pdata->edge_wakeup_register_once || !pdata->edge_wakeup_register_count) {
-				request_mfp_edge_wakeup(pdata->gpio_edge_wakeup,
-						dat1_edge_wakeup, NULL,
-						pdata->mmc->parent);
-				pdata->edge_wakeup_register_count = 1;
-			}
+			request_mfp_edge_wakeup(pdata->gpio_edge_wakeup,
+				dat1_edge_wakeup, NULL,
+				pdata->mmc->parent);
 		}
 
 		if (pdata->pinctrl && pdata->pin_on)
@@ -427,7 +424,7 @@ static int sd8x_pwr_off(struct sd8x_rfkill_platform_data *pdata)
 	/* Last: update the status */
 	pm_runtime_put_sync(host->mmc->parent);
 
-	if ((pdata->gpio_edge_wakeup >= 0) && (!pdata->edge_wakeup_register_once))
+	if (pdata->gpio_edge_wakeup >= 0)
 		remove_mfp_edge_wakeup(pdata->gpio_edge_wakeup);
 
 	if (pdata->set_power)
@@ -631,11 +628,6 @@ static int sd8x_rfkill_probe_dt(struct platform_device *pdev)
 		}
 	} else {
 		pdata->wib_sdio_1v8 = wib_sdio_1v8;
-	}
-
-	if (of_property_read_bool(np, "marvell,sdh-edge-wakeup-register-once")) {
-		pdata->edge_wakeup_register_count = 0;
-		pdata->edge_wakeup_register_once = true;
 	}
 
 	return 0;
